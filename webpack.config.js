ae703d76186e29bb3f02e45c53c0a35dd843b338
YevgeniyShunevych <eshunevich@gmail.com>
2016-05-22 15:17:32 +0300

    Add package.json and webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+﻿module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname + '/build/',
+        publicPath: 'build/',
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+            { test: /\.html$/, loader: "html" }
+        ]
+    },
+    babel: {
+        presets: ['es2015'],
+        plugins: ['transform-runtime']
+    }
+};
