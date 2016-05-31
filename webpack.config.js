d4066b77a05c918873f148e12808722fa4986faa
YevgeniyShunevych <eshunevich@gmail.com>
2016-05-31 22:32:23 +0300

    Fix woff,woff2 and ttf file errors

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
     entry: ['./src/index.js'],
     output: {
         path: __dirname + '/build/',
-        publicPath: 'build/',
         filename: 'build.js'
     },
     module: {
