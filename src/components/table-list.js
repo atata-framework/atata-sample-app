24ed7bb241bb37cefc0bb92c0fbfdee531033c02
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-01-21 16:44:00 +0200

    Update table-list page

--- src/components/table-list.js
+++ src/components/table-list.js
@@ -3,10 +3,11 @@
     data() {
         var items = [];
 
-        for (var i = 0; i < 250; i++) {
+        for (var i = 0; i < 500; i++) {
             items.push({
                 id: i + 1,
-                name: 'Item ' + (i + 1)
+                name: 'Item ' + (i + 1),
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non.'
             });
         }
         return {
